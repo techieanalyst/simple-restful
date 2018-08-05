a1755c105ba563c458adb47d172f213566c138e9
Roen Ylagan <roenylagan@live.com>
2018-08-05 21:26:57 +0800

    initial commit

---
+++ src/main/java/com/simple/restful/simplerestful/SimpleRestfulApplication.java
@@ -0,0 +1,12 @@
+package com.simple.restful.simplerestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestfulApplication.class, args);
+	}
+}
